84c1482725b6f33bd86a209acab7e46ebe2a6ee8
ciwwan1980 <ajilqado@gmail.com>
2024-03-02 01:02:43 +0100

    add model post catagory-register in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Category, Post
+admin.site.register(Category)
+admin.site.register(Post)
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 # blog/urls.py
 from django.urls import path
 from .views import blog_home
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', blog_home, name='blog_home'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
