442c7e11db946d63331a9be4caef0eeada710d6a
ciwwan1980 <ajilqado@gmail.com>
2024-03-02 02:00:05 +0100

    Add post_list view to display all posts&&Configure template directories to include BASE_DIR/templates&&Define URL pattern for the root path as requested by the user&&HTML template for displaying a list of blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 # blog/urls.py
 from django.urls import path
-from .views import blog_home
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
+from .views import post_list
+
+
 urlpatterns = [
-    path('', blog_home, name='blog_home'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.post_list, name='post_list'),
+]
