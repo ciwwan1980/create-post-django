c37c895b12a171df29e0f2631f5d60e27ee44d99
ciwwan1980 <ajilqado@gmail.com>
2024-03-03 16:00:37 +0100

    add forms to handle user request-style css

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
 # blog/urls.py
 from django.urls import path
-from . import views
-from .views import post_list
-
+from .views import post_list, add_post  # Ensure that add_post is correctly imported
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', post_list, name='post_list'),
+    path('add_post/', add_post, name='add_post'),
 ]
